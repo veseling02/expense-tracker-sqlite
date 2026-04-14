981a3bde19faf2405eeb363bf30ea9fc096aae9b
veseling02 <veskog43@gmail.com>
2026-04-14 12:41:15 +0300

    Connect and create table

---
+++ database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+table_statements =""" 
+CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY,
+    type TEXT NOT NULL,
+    amount REAL NOT NULL,
+    date DATE)
+                """
+
+try:
+    with sqlite3.connect("transactions.db") as conn:
+        print(f"Opened SQlite database with version {sqlite3.sqlite_version} successfully")
+        cursor = conn.cursor()
+        cursor.execute(table_statements)
+        print("TABLE CREATED SUCCESSFULLY")
+
+
+except sqlite3.OperationalError as e:
+    print("Failed to open database", e)
+print(conn)
